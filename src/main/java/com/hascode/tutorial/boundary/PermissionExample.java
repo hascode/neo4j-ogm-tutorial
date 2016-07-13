d7d3e8ea102057085bdf4f1ea6268b7b30f3cc51
Micha Kops <micha.kops@gmail.com>
2016-07-13 10:02:21 +0200

    Migrated to new OGM version and embedded Neo4j database.

--- src/main/java/com/hascode/tutorial/boundary/PermissionExample.java
+++ src/main/java/com/hascode/tutorial/boundary/PermissionExample.java
@@ -5,64 +5,67 @@ import java.util.Collections;
 import org.neo4j.ogm.session.Session;
 import org.neo4j.ogm.session.SessionFactory;
 
+import com.hascode.tutorial.entity.Group;
 import com.hascode.tutorial.entity.Permission;
+import com.hascode.tutorial.entity.Role;
+import com.hascode.tutorial.entity.User;
 
 public class PermissionExample {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SessionFactory sessionFactory = new SessionFactory("com.hascode");
-		final Session session = sessionFactory.openSession("http://localhost:7474");
+        SessionFactory sessionFactory = new SessionFactory("com.hascode");
+        final Session session = sessionFactory.openSession();
 
-		// Permission canRead = new Permission("permission-read");
-		// Permission canWrite = new Permission("permission-write");
-		// Permission canDelete = new Permission("permission-delete");
-		//
-		// Role adminRole = new Role("administrator");
-		// Role editorRole = new Role("editor");
-		// Role guestRole = new Role("guest");
-		//
-		// Group group1 = new Group("group1");
-		// Group group2 = new Group("group2");
-		// Group group3 = new Group("group3");
-		//
-		// User fred = new User("Fred");
-		// User sally = new User("Sally");
-		// User lisa = new User("Lisa");
-		//
-		// adminRole.addPermission(canRead);
-		// adminRole.addPermission(canWrite);
-		// adminRole.addPermission(canDelete);
-		//
-		// editorRole.addPermission(canRead);
-		// editorRole.addPermission(canWrite);
-		//
-		// guestRole.addPermission(canRead);
-		//
-		// group1.addPermission(canRead);
-		// group1.addPermission(canWrite);
-		// group2.addPermission(canRead);
-		// group3.addPermission(canRead);
-		//
-		// group1.addRole(editorRole);
-		// group2.addRole(adminRole);
-		//
-		// fred.addToGroup(group1);
-		// sally.addToGroup(group2);
-		// lisa.addToGroup(group3);
-		//
-		// session.save(fred);
-		// session.save(sally);
-		// session.save(lisa);
+        Permission canRead = new Permission("permission-read");
+        Permission canWrite = new Permission("permission-write");
+        Permission canDelete = new Permission("permission-delete");
 
-		fetchPermissionFor(session, "Fred");
-		fetchPermissionFor(session, "Sally");
-		fetchPermissionFor(session, "Lisa");
-	}
+        Role adminRole = new Role("administrator");
+        Role editorRole = new Role("editor");
+        Role guestRole = new Role("guest");
 
-	private static void fetchPermissionFor(final Session session, String name) {
-		System.out.println("fetching permissions for " + name + ": ");
-		session.query(Permission.class, "MATCH (u:User{name:'" + name + "'})-[*]->(p:Permission) RETURN p",
-				Collections.emptyMap()).forEach(System.out::println);
-	}
+        Group group1 = new Group("group1");
+        Group group2 = new Group("group2");
+        Group group3 = new Group("group3");
+
+        User fred = new User("Fred");
+        User sally = new User("Sally");
+        User lisa = new User("Lisa");
+
+        adminRole.addPermission(canRead);
+        adminRole.addPermission(canWrite);
+        adminRole.addPermission(canDelete);
+
+        editorRole.addPermission(canRead);
+        editorRole.addPermission(canWrite);
+
+        guestRole.addPermission(canRead);
+
+        group1.addPermission(canRead);
+        group1.addPermission(canWrite);
+        group2.addPermission(canRead);
+        group3.addPermission(canRead);
+
+        group1.addRole(editorRole);
+        group2.addRole(adminRole);
+
+        fred.addToGroup(group1);
+        sally.addToGroup(group2);
+        lisa.addToGroup(group3);
+
+        session.save(fred);
+        session.save(sally);
+        session.save(lisa);
+
+        fetchPermissionFor(session, "Fred");
+        fetchPermissionFor(session, "Sally");
+        fetchPermissionFor(session, "Lisa");
+    }
+
+    private static void fetchPermissionFor(final Session session, String name) {
+        System.out.println("fetching permissions for " + name + ": ");
+        session.query(Permission.class, "MATCH (u:User{name:'" + name + "'})-[*]->(p:Permission) RETURN p",
+                Collections.emptyMap()).forEach(System.out::println);
+    }
 
 }
--- src/main/java/com/hascode/tutorial/boundary/TrainTimetableExample.java
+++ src/main/java/com/hascode/tutorial/boundary/TrainTimetableExample.java
@@ -8,54 +8,54 @@ import org.neo4j.ogm.session.SessionFactory;
 import com.hascode.tutorial.entity.TrainStation;
 
 public class TrainTimetableExample {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SessionFactory sessionFactory = new SessionFactory("com.hascode");
-		final Session session = sessionFactory.openSession("http://localhost:7474");
+        SessionFactory sessionFactory = new SessionFactory("com.hascode");
+        final Session session = sessionFactory.openSession();
 
-		TrainStation london = new TrainStation("London");
-		TrainStation brighton = new TrainStation("Brighton");
-		TrainStation portsmouth = new TrainStation("Portsmouth");
-		TrainStation bristol = new TrainStation("Bristol");
-		TrainStation oxford = new TrainStation("Oxford");
-		TrainStation gloucester = new TrainStation("Gloucester");
-		TrainStation northampton = new TrainStation("Northampton");
-		TrainStation southampton = new TrainStation("Southampton");
+        TrainStation london = new TrainStation("London");
+        TrainStation brighton = new TrainStation("Brighton");
+        TrainStation portsmouth = new TrainStation("Portsmouth");
+        TrainStation bristol = new TrainStation("Bristol");
+        TrainStation oxford = new TrainStation("Oxford");
+        TrainStation gloucester = new TrainStation("Gloucester");
+        TrainStation northampton = new TrainStation("Northampton");
+        TrainStation southampton = new TrainStation("Southampton");
 
-		london.addConnection(brighton, 52);
-		brighton.addConnection(portsmouth, 49);
-		portsmouth.addConnection(southampton, 20);
-		london.addConnection(oxford, 95);
-		oxford.addConnection(southampton, 66);
-		oxford.addConnection(northampton, 45);
-		northampton.addConnection(bristol, 114);
-		southampton.addConnection(bristol, 77);
-		northampton.addConnection(gloucester, 106);
-		gloucester.addConnection(bristol, 35);
+        london.addConnection(brighton, 52);
+        brighton.addConnection(portsmouth, 49);
+        portsmouth.addConnection(southampton, 20);
+        london.addConnection(oxford, 95);
+        oxford.addConnection(southampton, 66);
+        oxford.addConnection(northampton, 45);
+        northampton.addConnection(bristol, 114);
+        southampton.addConnection(bristol, 77);
+        northampton.addConnection(gloucester, 106);
+        gloucester.addConnection(bristol, 35);
 
-		session.save(london);
-		session.save(brighton);
-		session.save(portsmouth);
-		session.save(bristol);
-		session.save(oxford);
-		session.save(gloucester);
-		session.save(northampton);
-		session.save(southampton);
+        session.save(london);
+        session.save(brighton);
+        session.save(portsmouth);
+        session.save(bristol);
+        session.save(oxford);
+        session.save(gloucester);
+        session.save(northampton);
+        session.save(southampton);
 
-		System.out.println(session.countEntitiesOfType(TrainStation.class) + " stations saved");
-		getRoute("London", "Bristol", session);
-		getRoute("London", "Southampton", session);
-	}
+        System.out.println(session.countEntitiesOfType(TrainStation.class) + " stations saved");
+        getRoute("London", "Bristol", session);
+        getRoute("London", "Southampton", session);
+    }
 
-	private static void getRoute(final String from, final String destination, final Session session) {
-		System.out.println("searching for the shortest route from " + from + " to " + destination + "..");
-		final Iterable<TrainStation> stops = session.query(TrainStation.class,
-				"MATCH (from:TrainStation {name:'" + from + "'}), (to:TrainStation {name:'" + destination
-						+ "'}), path=shortestPath((from)-[:LEADS_TO*]->(to)) RETURN path",
-				Collections.<String, TrainStation> emptyMap());
-		System.out.println("shortest way from " + from + " to " + destination + " via");
-		stops.forEach((stop) -> {
-			System.out.println(stop.getName());
-		});
-	}
+    private static void getRoute(final String from, final String destination, final Session session) {
+        System.out.println("searching for the shortest route from " + from + " to " + destination + "..");
+        final Iterable<TrainStation> stops = session.query(TrainStation.class,
+                "MATCH (from:TrainStation {name:'" + from + "'}), (to:TrainStation {name:'" + destination
+                        + "'}), path=shortestPath((from)-[:LEADS_TO*]->(to)) RETURN path",
+                Collections.<String, TrainStation> emptyMap());
+        System.out.println("shortest way from " + from + " to " + destination + " via");
+        stops.forEach((stop) -> {
+            System.out.println(stop.getName());
+        });
+    }
 }
